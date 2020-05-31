8cb9556578e31a6bf077cec05a45ac411e6e87cc
SweetieMetalhead <paul.lv.ohl@gmail.com>
2020-05-31 16:37:18 +0200

    Continued translation and added .gitignore

--- lang.js
+++ lang.js
@@ -1,26 +1,59 @@
 i18next.init({
 	lng: 'en', // evtl. use language-detector https://github.com/i18next/i18next-browser-languageDetector
+	fallbackLng: ['constants', 'en', 'fr'],
 	resources: {
+		constants: {
+			translation: {
+				author: {
+					name: "Paul OHL",
+					mail: {
+						link: "mailto:paul.lv.ohl@gmail.com",
+						value: "paul.lv.ohl@gmail.com"
+					},
+					phone: {
+						link: "tel:+33781196316",
+						value: "07.81.19.63.16"
+					},
+					linkedin: {
+						link: "https://www.linkedin.com/in/paul-ohl-19aaab196/",
+						value: "Paul OHL"
+					},
+					github: {
+						link: "https://github.com/popohl",
+						value: "pohl"
+					},
+				}
+			}
+		},
 		en: {
 			translation: {
 				nav: {
 					contact: "Contact me",
 					mail: "Mail: ",
-					phone: "Phone: "
+					phone: "Phone: ",
+					linkedin: "Linkedin: ",
+					github: "Github: "
 				},
-				me: {
-					name: "Paul OHL",
-					job: "Digital technologies architect",
-					mail: {
-						link: "mailto:paul.lv.ohl@gmail.com",
-						view: "paul.lv.ohl@gmail.com"
+				author: {
+					job: "Digital technologies architect"
+				},
+				sections: {
+					profile: {
+						title: "My Profile",
+						text: "I am always looking for experiences that will expand my horizons as a developper. I am willing to work hard in order to acquire skills that will allow me to improve myself, and give meaning to what I do."
 					},
-					phone: {
-						link: "tel:+33781196316",
-						view: "07.81.19.63.16"
+					education: {
+						title: "Education",
 					},
-					linkedin: "https://www.linkedin.com/in/paul-ohl-19aaab196/",
-					github: "https://github.com/popohl"
+					experience: {
+						title: "Experience",
+					},
+					software_skills: {
+						title: "Software skills",
+					},
+					other_skills: {
+						title: "Other skills",
+					}
 				}
 			}
 		},
@@ -28,22 +61,35 @@ i18next.init({
 			translation: {
 				nav: {
 					contact: "Contactez moi",
-					mail: "E-mail: ",
-					phone: "Téléphone: "
+					mail: "E-mail : ",
+					phone: "Téléphone : ",
+					linkedin: "Linkedin : ",
+					github: "Github : "
 				},
-				me: {
-					name: "Paul OHL",
-					job: "Français",
-					mail: {
-						link: "mailto:paul.lv.ohl@gmail.com",
-						view: "paul.lv.ohl@gmail.com"
+				author: {
+					job: "Architecte en technologies du numérique"
+				},
+				sections: {
+					profile: {
+						title: "Mon Profil",
+						text: "Je recherche avant tout des expériences qui me permettent d'avancer dans mon parcours de développeur. Je suis prêt à travailler dur pour acquérir des compétences qui me permettront de m'améliorer et de donner du sens à ce que je fais."
 					},
-					phone: {
-						link: "tel:+33781196316",
-						view: "07.81.19.63.16"
+					education: {
+						title: "Éducation",
+						
 					},
-					linkedin: "https://www.linkedin.com/in/paul-ohl-19aaab196/",
-					github: "https://github.com/popohl"
+					experience: {
+						title: "Expérience",
+						
+					},
+					software_skills: {
+						title: "Compétences en programmation",
+						
+					},
+					other_skills: {
+						title: "Autres compétences",
+
+					}
 				}
 			}
 		}
@@ -54,7 +100,7 @@ i18next.init({
 });
 
 function updateContent() {
-	localize('nav');
+	localize('body');
 }
 
 function changeLng(lng) {
@@ -64,3 +110,17 @@ function changeLng(lng) {
 i18next.on('languageChanged', () => {
 	updateContent();
 });
+
+var author = {
+	name: "Paul OHL",
+	mail: {
+		link: "mailto:paul.lv.ohl@gmail.com",
+		value: "paul.lv.ohl@gmail.com"
+	},
+	phone: {
+		link: "tel:+33781196316",
+		value: "07.81.19.63.16"
+	},
+	linkedin: "https://www.linkedin.com/in/paul-ohl-19aaab196/",
+	github: "https://github.com/popohl"
+}
